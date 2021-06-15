0dc986a2499d7dbf70eb2930701c1ad4123ba00e
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-06-15 21:58:49 +0200

    Remove logic from the MarketEnvironment.

--- crypto_ml_trader_trainer/pytorch_based/trader/environments/market_environment.py
+++ crypto_ml_trader_trainer/pytorch_based/trader/environments/market_environment.py
@@ -1,13 +1,12 @@
 import logging
 from logging import Logger
+from typing import Dict, Optional, Tuple
 
 import gym
 import numpy as np
-import pandas as pd
 from gym import spaces
 
-from pytorch_based.trader.environments.logic.market_env_logic import MarketEnvLogic
-from pytorch_based.trader.environments.logic.market_tick_indicators_env_logic import MarketTickIndicatorsEnvLogic
+from pytorch_based.trader.environments.market_env_logic import MarketEnvLogic
 from pytorch_based.trader.environments.market_plot import MarketPlot
 from shared.environments.trading_action import TradingAction
 
@@ -16,66 +15,67 @@ class MarketEnvironment(gym.Env):
     logger: Logger = logging.getLogger(__name__)
 
     max_net_worth = 0
-    render_type = "none"
     print_market_action_frequency = 500
+
 
     def __init__(self, market_logic: MarketEnvLogic):
         super().__init__()
 
-        self.market_logic = market_logic
+        self.market_logic: MarketEnvLogic = market_logic
         self.market_logic.logger = self.logger
 
         self.action_space = spaces.Discrete(3)
-        self.observation_space = spaces.Box(shape=self.market_logic.next().shape, dtype=np.double, low=-1.0, high=1.0)
+        self.observation_space = spaces.Box(shape=self.market_logic.next()[0].shape, dtype=np.double, low=-1.0, high=1.0)
 
         self._set_initial_state()
         self.logger.level = logging.INFO
         self.logger.disabled = True
-        self.plot = MarketPlot()
+        self.market_plot: Optional[MarketPlot] = None
 
-    def _set_initial_state(self):
+
+    def _set_initial_state(self) -> np.array:
         self.market_logic.reset()
-        state = self.market_logic.next()
+        state, _, _ = self.market_logic.next()
         self._episode_ended = False
         self.max_net_worth = self.market_logic.net_worth()
         return state
 
-    #region Environment Interaction
 
-    def step(self, action: spaces.Discrete):
+    # region Environment Interaction
+
+
+    def step(self, action: spaces.Discrete) -> Tuple[np.array, float, float, Dict]:
         if self._episode_ended:
             # The last action ended the episode. Ignore the current action and start
             # a new episode.
             raise Exception("The episode is ended. Reset it before calling step()")
 
-        new_state = self.market_logic.next()
+        self.market_logic.execute_action(TradingAction(action))
+        new_state, reward, early_end = self.market_logic.next()
 
-        if not self.market_logic.has_next():
+        if not self.market_logic.has_next() or early_end:
             self._episode_ended = True
 
-        # Stop if loosing too much!
-        if self.market_logic.net_worth() < 0.75 * self.max_net_worth:
-            reward = -100
-            self._episode_ended = True
 
-        if self.max_net_worth < self.market_logic.net_worth():
-            self.max_net_worth = self.market_logic.net_worth()
-
-        reward = self.market_logic.execute_action(TradingAction(action))
-
-        self.plot.add(self.market_logic.current_date(), self.market_logic.current_price(), TradingAction(action))
+        if self.market_plot is not None:
+            self.market_plot.add(self.market_logic.current_date(), self.market_logic.current_price(),
+                                 TradingAction(action))
 
         if self._episode_ended:
             return new_state, reward, True, {}
 
         return new_state, reward, False, {}
 
+
     def valid_moves(self) -> [TradingAction]:
         return self.market_logic.valid_moves()
 
+
     def reset(self):
-        self.plot.save_reset_plot()
+        if self.market_plot is not None:
+            self.market_plot.save_reset_plot()
         return self._set_initial_state()
+
 
     def render(self, mode='human'):
         print("-------------")
@@ -86,7 +86,9 @@ class MarketEnvironment(gym.Env):
         print(f"Profits: {self.market_logic.wallet.profits()}")
         print(f"Progress: {self.market_logic.episode_progress() * 100} %")
 
-        if self.render_type == "plot":
-            self.plot.plot()
+        if mode == "plot":
+            if self.market_plot is None:
+                self.market_plot = MarketPlot()
+            self.market_plot.plot()
 
-    #end region
+    # endregion
