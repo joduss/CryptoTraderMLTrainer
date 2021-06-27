50a248af8f3dbb4aa574034301d76e5f22c7a740
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-06-27 19:19:07 +0200

    Improve Data preparation and preprocessing for the MarketTickIndicatorEnv.

--- crypto_ml_trader_trainer/pytorch_based/trader/environments/current_tick_indicators/market_tick_indicator_data.py
+++ crypto_ml_trader_trainer/pytorch_based/trader/environments/current_tick_indicators/market_tick_indicator_data.py
@@ -1,41 +1,52 @@
 import json
 import os
-from typing import List
 
+import numpy as np
 import pandas as pd
 import pandas_ta
-
+import utilities.pandas_extension_scaler
 
 class MarketTickIndicatorData:
 
-    _cached_data_path = "./cache/MarketTickIndicatorData.json"
-
     @classmethod
-    def prepare_data(cls, data_to_process: pd.DataFrame, index_jump: int) -> List:
+    def prepare_data(cls, data_to_process: pd.DataFrame, index_jump: int) -> np.array:
 
-        if os.path.isfile(cls._cached_data_path):
-            with open(cls._cached_data_path, 'r') as file:
-                return json.load(file)
+        cached_data_path = f"./cache/MarketTickIndicatorData-{index_jump}.json"
 
+        # Loads from cache
+        if os.path.isfile(cached_data_path):
+            with open(cached_data_path, 'r') as file:
+                return np.array(json.load(file))
+
+        # Create data and save it to cache
         data = data_to_process.copy()
 
-        macd_period = 5
+        # All periods/lengths are in minutes
+        macd_period = 10
 
         data["ma-5min"] = data["close"].rolling(window=5).mean()
         data["ma-30min"] = data["close"].rolling(window=30).mean()
         data["ma-180min"] = data["close"].rolling(window=180).mean()
         data["ma-360min"] = data["close"].rolling(window=360).mean()
-        data["rsi-14days"] = data.ta.rsi(length=14 * 60 * 24) / 100
+        data["ma-1d"] = data["close"].rolling(window=1440).mean()
+        data["ma-1d"] = data["close"].rolling(window=1440).mean()
+        data["ma-3d"] = data["close"].rolling(window=3*1440).mean()
+        data["rsi-14d"] = data.ta.rsi(length=14 * 60 * 24) / 100
         data["rsi-14h"] = data.ta.rsi(length=14 * 60) / 100
-        data["rsi-3h"] = data.ta.rsi(length=3 * 60)
+        data["rsi-3h"] = data.ta.rsi(length=3 * 60) / 100
         data["ema-12"] = data.ta.ema(length=12 * macd_period)
         data["ema-26"] = data.ta.ema(length=26 * macd_period)
         data["rvi"] = data.ta.rvi() / 100
+        data["cci-1day"] = data.ta.cci(length=1440).scaler.normalize_01()
+        data["cci-3days"] = data.ta.cci(length=3*1440).scaler.normalize_01()
+        data["cci-6h"] = data.ta.cci(length=360).scaler.normalize_01()
+        data["willr-1d"] = data.ta.willr(length=24*60*5).scaler.normalize_01()
 
         data = data.join(data.ta.bbands(length=120))
-        data = data.join(data.ta.stoch(length=60))
+        data = data.join(data.ta.stoch(length=60) / 100)
+        data = data.drop(columns="BBB_120_2.0")
 
-        data["macd"] = data["ema-26"] - data["ema-12"]
+        data["macd"] = (data["ema-26"] - data["ema-12"])
         for_signal = pd.DataFrame(data["macd"])
         for_signal.rename(inplace=True, columns={"macd": "close"})
 
@@ -44,17 +55,20 @@ class MarketTickIndicatorData:
         data = data.dropna()
 
         # Determine which column to normalize / scale
-        scaled_data = []
+        # scaled_data = []
         cols_to_normalize_template: [str] = ["ma-", "ema-", "close", "high", "low", "open", "BBU", "BBM", "BBL"]
         cols_to_normalize: [str] = []
 
-        for col in [col for col in data.columns for col_to_norm in cols_to_normalize_template if
-                    col.startswith(col_to_norm)]:
+        for col in [col for col in data.columns for col_to_norm in cols_to_normalize_template
+                    if col.startswith(col_to_norm)]:
             cols_to_normalize.append(col)
 
-        for idx in range(0, len(data), index_jump):
-            time_idx = data.index[idx]
-            row = data.iloc[idx]
+        indices = range(0, len(data), index_jump)
+        filtered_data = pd.DataFrame(data.iloc[indices])
+
+        for idx in range(0, len(indices)):
+            time_idx = filtered_data.index[idx]
+            row = filtered_data.iloc[idx]
             close = row["close"]
 
             field_to_normalize = row[cols_to_normalize]
@@ -64,19 +78,33 @@ class MarketTickIndicatorData:
             max_diff = max(abs(max_value - close), abs(close - min_value))
 
             for col in cols_to_normalize:
-                data.at[time_idx, col] = cls._scale(row[col], close, max_diff)
+                filtered_data.at[time_idx, col] = cls._scale(row[col], close, max_diff)
 
-            scaled_data.append(row.to_list())
+            # scaled_data.append(row.to_list())
 
-            if idx % (100 * index_jump) == 0:
-                print(f"{idx}/{len(data)}")
+            if idx % (100) == 0:
+                print(f"{idx}/{len(filtered_data)}")
+
+
+        # macd features scaling
+        min_value = min(filtered_data["macd"].min(), filtered_data["macd-signal"].min())
+        max_value = max(filtered_data["macd"].max(), filtered_data["macd-signal"].max())
+        filtered_data["macd"] = filtered_data["macd"].scaler.normalize_01(min=min_value, max=max_value)
+        filtered_data["macd-signal"] = filtered_data["macd-signal"].scaler.normalize_01(min=min_value, max=max_value)
+        filtered_data["macd-hist"] = filtered_data["macd-hist"].scaler.normalize_01()
+
+        # dropping volume and count
+        filtered_data = filtered_data.drop(columns="volume")
+        filtered_data = filtered_data.drop(columns="trades")
 
         print("Data prepared")
 
-        with open(cls._cached_data_path, 'w') as file:
-            file.write(json.dumps(scaled_data))
+        filtered_data = filtered_data.to_numpy()
 
-        return scaled_data
+        with open(cached_data_path, 'w') as file:
+            file.write(json.dumps(filtered_data.tolist()))
+
+        return filtered_data
 
 
     @classmethod
