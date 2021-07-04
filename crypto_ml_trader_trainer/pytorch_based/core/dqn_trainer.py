Fix major bug which prevented the zeroing of the gradient values.
- The model the optimizer got the parameters was the target_network instead of the policy_network.

crypto_ml_trader_trainer/pytorch_based/core/dqn_trainer.py
@@ -44,8 +44,8 @@ class DQNTrainer:
                  parameters: DQNTrainerParameters,
                  policy: Policy):
         self.memory = ReplayMemory(parameters.memory_size)
-        self.target_net: nn.Module = model
-        self.policy_net: nn.Module = copy.deepcopy(model)
+        self.policy_net: nn.Module = model
+        self.target_net: nn.Module = copy.deepcopy(model)
         self.environment = environment
         self.optimizer = optimizer
         self.parameters = parameters
@@ -164,10 +164,10 @@ class DQNTrainer:
         expected_state_action_values = (next_state_values * self.gamma) + reward_batch
 
         # Compute Huber loss
+        self.optimizer.zero_grad()
         loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
 
         # Optimize the model
-        self.optimizer.zero_grad()
         loss.backward()
         for param in self.policy_net.parameters():
             param.grad.data.clamp_(-1, 1)

crypto_ml_trader_trainer/pytorch_based/trader/pytorch_trader_v1.py
@@ -60,7 +60,7 @@ def run(data_file_path = None):
     # environment_val = MarketIndicatorTfEnvironment(data_val)
 
     model = MarketIndicatorNN(input_length=wrapped_env.observation_space.shape[1])
-    optimizer = torch.optim.RMSprop(model.network.parameters(), lr = 0.01)
+    optimizer = torch.optim.RMSprop(model.parameters(), lr = 0.01)
     # policy = TraderPolicy(env, model)
     policy = GreedyPolicy(num_actions=3, policy_net=model, eps_decay=30000, decay_per_episode=False)
 